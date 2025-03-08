1ad3489cdd1a8100fdba3ef0a319b0ea24e93740
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2025-03-08 05:07:44 -0400

    chore: set up base project

---
+++ src/main/java/degree/project/usb/pocbusinessintelligence/PocBusinessIntelligenceApplication.java
@@ -0,0 +1,13 @@
+package degree.project.usb.pocbusinessintelligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocBusinessIntelligenceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PocBusinessIntelligenceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/degree/project/usb/pocbusinessintelligence/PocBusinessIntelligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package degree.project.usb.pocbusinessintelligence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocBusinessIntelligenceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
